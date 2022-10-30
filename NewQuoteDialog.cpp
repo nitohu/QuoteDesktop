564a358a6b16daeef6aab3156c38209a58dfa19b
nitohu <nitohu@gmail.com>
2022-10-30 13:51:59 +0100

    [IMP] Create quote dialog

--- NewQuoteDialog.cpp
+++ NewQuoteDialog.cpp
@@ -4,27 +4,28 @@
 
 NewQuoteDialog::NewQuoteDialog(wxWindow *parent)
     : wxDialog(parent, wxID_ANY, "Create new quote") {
-        wxBoxSizer *top = new wxBoxSizer(wxVERTICAL);
-        wxBoxSizer *btn_bar = new wxBoxSizer(wxHORIZONTAL);
+        wxFlexGridSizer *top = new wxFlexGridSizer(2, 3, 3);
         
-       m_quoteTxt = new wxTextCtrl(this, wxID_ANY);
-       m_authorTxt = new wxTextCtrl(this, wxID_ANY);
+        wxStaticText *quoteLabel = new wxStaticText(this, wxID_ANY, "Quote");
+        m_quoteTxt = new wxTextCtrl(this, wxID_ANY, wxEmptyString, wxDefaultPosition, wxSize(400, -1));
+        top->Add(quoteLabel);
+        top->Add(m_quoteTxt);
 
-       m_createBtn = new wxButton(this, ID_CreateQuote, "Create");
-       m_discardBtn = new wxButton(this, wxID_CLOSE, "Discard");
+        wxStaticText *authorLabel = new wxStaticText(this, wxID_ANY, "Author / Source");
+        m_authorTxt = new wxTextCtrl(this, wxID_ANY, wxEmptyString, wxDefaultPosition, wxSize(400, -1));
+        top->Add(authorLabel);
+        top->Add(m_authorTxt);
 
-       btn_bar->Add(m_createBtn);
-       btn_bar->Add(m_discardBtn);
+        m_createBtn = new wxButton(this, ID_CreateQuote, "Create");
+        m_discardBtn = new wxButton(this, wxID_CLOSE, "Discard");
+        top->Add(m_createBtn);
+        top->Add(m_discardBtn);
 
-       top->Add(m_quoteTxt);
-       top->Add(m_authorTxt);
-       top->Add(btn_bar);
+        SetSizerAndFit(top);
+        SetEscapeId(wxID_CLOSE);
 
-       SetSizerAndFit(top);
-       SetEscapeId(wxID_CLOSE);
-
-       Bind(wxEVT_BUTTON, &NewQuoteDialog::OnCreateQuote, this, ID_CreateQuote);
-       m_discardBtn->Bind(wxEVT_BUTTON, &NewQuoteDialog::OnDiscardQuote, this);
+        Bind(wxEVT_BUTTON, &NewQuoteDialog::OnCreateQuote, this, ID_CreateQuote);
+        m_discardBtn->Bind(wxEVT_BUTTON, &NewQuoteDialog::OnDiscardQuote, this);
 };
 
 void NewQuoteDialog::OnCreateQuote(wxCommandEvent &evt) {
